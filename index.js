eb59a4f390fed0de1b6629b6bf50935f2e101e6b
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-04-01 22:25:44 +0530

    removed unexpected try catch

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 export default function responseDataUnpacker(axiosConfig) {
-	//This interceptor must be last
-	try {
-		if (axiosConfig.config.unpackResponseData) {
-			axiosConfig = axiosConfig.data;
-		}
-	} catch (error) {
-		//suppress
+	if (axiosConfig.config.unpackResponseData) {
+		axiosConfig = axiosConfig.data;
 	}
+
 	return axiosConfig;
 }
