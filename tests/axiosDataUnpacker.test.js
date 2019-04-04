3d314e9ee53ff01bbc6ba6d377f59bdbb5c16443
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-04-04 22:44:04 +0530

    added test cases

--- tests/axiosDataUnpacker.test.js
+++ tests/axiosDataUnpacker.test.js
@@ -1,37 +1,28 @@
-const module = require('../src/index');
+import responseDataUnpacker, { axiosResponseDataUnpacker } from '../src/index';
+import { JestEnvironment } from '@jest/environment';
 
 describe('axios interceptor - data unpacker', () => {
 	test('should add itself in interceptor chain of axios', () => {
-		// Object.defineProperty(window.navigator, 'userAgent', { value: 'browser', configurable: true });
-		// const wrapper = shallow(
-		// 	<ElectronOnly>
-		// 		<div className="content">Content exclusively for Electron</div>
-		// 	</ElectronOnly>
-		// );
-		// expect(wrapper.find('div.content').length).toEqual(0);
-		// expect(wrapper.children().length).toEqual(0);
+		let axiosMock = {
+			interceptors: {
+				response: {
+					use: jest.fn()
+				}
+			}
+		};
+		axiosResponseDataUnpacker(axiosMock);
+		expect(axiosMock.interceptors.response.use).toBeCalledWith(responseDataUnpacker);
 	});
 
-	test('should render for electron', () => {
-		// Object.defineProperty(window.navigator, 'userAgent', { value: 'Electron', configurable: true });
-		// const wrapper = shallow(
-		// 	<ElectronOnly>
-		// 		<div className="content">Content exclusively for Electron</div>
-		// 	</ElectronOnly>
-		// );
-		// expect(wrapper.find('div.content').length).toEqual(1);
-		// expect(wrapper.children().length).toEqual(1);
+	test('should change the object to object.data (unpacking)', () => {
+		let axiosConfigResponse = { config: {}, data: { a: 1 } };
+		const unpackedConfig = responseDataUnpacker(axiosConfigResponse);
+		expect(unpackedConfig).toBe(axiosConfigResponse.data);
 	});
 
-	test('should render for browser with default fallbackComponent prop', () => {
-		// Object.defineProperty(window.navigator, 'userAgent', { value: 'browser', configurable: true });
-		// const wrapper = shallow(
-		// 	<ElectronOnly fallbackComponent={<div className="browser-content">Content exclusively for Browser</div>}>
-		// 		<div className="content">Content exclusively for Electron</div>
-		// 	</ElectronOnly>
-		// );
-		// expect(wrapper.find('div.content').length).toEqual(0);
-		// expect(wrapper.find('div.browser-content').length).toEqual(1);
-		// expect(wrapper.children().length).toEqual(1);
+	test('should not change the object to object.data using packResponseData flag', () => {
+		let axiosConfigResponse = { config: { packResponseData: true }, data: { a: 1 } };
+		const unpackedConfig = responseDataUnpacker(axiosConfigResponse);
+		expect(unpackedConfig).not.toBe(axiosConfigResponse.data);
 	});
 });
